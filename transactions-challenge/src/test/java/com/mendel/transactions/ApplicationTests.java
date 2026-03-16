50f6ff25a763c66a7ceaac0ae1ae61d79d438455
mdolce75 <madolce75@gmail.com>
2026-03-16 14:33:06 -0300

    Add files via upload
    Initial commit

---
+++ transactions-challenge/src/test/java/com/mendel/transactions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendel.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
